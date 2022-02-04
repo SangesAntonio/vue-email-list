0c78a3fea96d43211e9ac3aee4d5ff4552c0a7f3
SangesAntonio <sanges65@gmail.com>
2022-02-04 16:21:19 +0100

    added 10 emails in a list

--- js/script.js
+++ js/script.js
@@ -2,3 +2,31 @@ console.log('js ok')
 console.log('vue ok', Vue)
 console.log('axios ok', axios)
 
+const app = new Vue({
+    el:'#root',
+    data:{
+        total: 10,
+        emails: [],
+    },
+    methods:{
+        getRandomEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res =>{
+                if(!this.emails.includes(res.data.response)){
+                    this.emails.push(res.data.response)
+                }
+                
+            });
+        },
+        getRandomEmails(){
+            for(let i=0; i < this.total; i++){
+                this.getRandomEmail()
+            }
+        },
+    },
+    created(){
+       this.getRandomEmails()
+    }
+
+
+})
+
